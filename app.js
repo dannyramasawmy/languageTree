2beef09c69962a35a9bf436859a988ec1162cf30
dramasawmy <dannyramasawmy@gmail.com>
2022-05-11 10:19:24 +0100

    some color

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-var searchable = new SearchableDictionary();
-GetSearchableWords(romanian, searchable);
-
-
-
-window.addEventListener('click',
-    function (event) {
-        var clickId = event.path[0].id;
-        var displayList = GetDisplayNodes(currentNode);
-        console.log(displayList)
-
-        if (displayList[clickId] !== undefined) {
-            currentNode = displayList[clickId];
-            var displayList = GetDisplayNodes(currentNode);
-            var elementName = "node-display";
-            ClearNodeDisplay(elementName)
-            PopulateNodeDisplay(elementName, headerName, dataName, currentNode, displayList)
-        }
-    })
-
+// =============================================================================
+// Global variables
+// =============================================================================
 
 const rootNode = romanian;
 const elementName = "node-display";
 const headerName = "current-node";
 const dataName = "current-node-data";
-var currentNode = rootNode;
+const colorWheel = new ColorWheel();
 
+// =============================================================================
+// Initialise
+// =============================================================================
+
+var currentNode = rootNode;
 var displayList = GetDisplayNodes(currentNode);
 ClearNodeDisplay(elementName)
 PopulateNodeDisplay(elementName, headerName, dataName, currentNode, displayList)
+
+var searchable = new SearchableDictionary();
+GetSearchableWords(romanian, searchable);
+
+// =============================================================================
+// Events
+// =============================================================================
+
+window.addEventListener('click',
+    function (event) {
+
+        var clickId = event.composedPath()[0].id;
+        var displayList = GetDisplayNodes(currentNode);
+
+        // update display
+        if (displayList[clickId] !== undefined) {
+            currentNode = displayList[clickId];
+            var displayList = GetDisplayNodes(currentNode);
+            var elementName = "node-display";
+            
+            ClearNodeDisplay(elementName)
+            PopulateNodeDisplay(elementName, headerName, dataName, currentNode, displayList)
+        }
+    })
--- src.js
+++ src.js
@@ -1,3 +1,7 @@
+// =============================================================================
+// Containers
+// =============================================================================
+
 function DataCard(english, romanian, data) {
     this.English = english;
     this.Romanian = romanian;
@@ -5,54 +9,58 @@ function DataCard(english, romanian, data) {
     
     this.Parent = [];
     this.Child = [];
-
+    
     this.AddChild = function(dataCard) {
         this.Child.push(dataCard);
     }
-
+    
     this.SetParent = function(dataCard) {
         this.Parent = dataCard;
     }
 }
-
 
 function SearchableDictionary() {
     this.Romanian = {};
     this.English = {};
 }
 
+// =============================================================================
+// Data Functions
+// =============================================================================
 
 function SetParentAndChild(parentDataCard, childDataCard) {
     parentDataCard.AddChild(childDataCard);
     childDataCard.SetParent(parentDataCard);
 }
 
-
 function GetSearchableWords(rootDataCard, searchableDictionary) {
     
     searchableDictionary.English[rootDataCard.English] = rootDataCard;
     searchableDictionary.Romanian[rootDataCard.Romanian] = rootDataCard;
-
+    
     if (rootDataCard.Child.length == 0) {
         return;
     }
-
+    
     for (let i = 0; i < rootDataCard.Child.length ; i++) {
         GetSearchableWords(rootDataCard.Child[i], searchableDictionary)
     }
     return;
 }
 
+// =============================================================================
+// Display Functions
+// =============================================================================
 
 function GetDisplayNodes(node) {
     var displayList = [];
-
+    
     displayList.push(node.Parent);
-
+    
     for (var idx = 0; idx < node.Child.length; idx++) {
         displayList.push(node.Child[idx])
     }
-
+    
     return displayList;
 }
 
@@ -78,7 +86,42 @@ function PopulateNodeDisplay(elementName, headerName, dataName, currentNode, dis
         newDiv.innerHTML = displayList[idx].English;
         newDiv.id = idx;
         newDiv.className = "data-card";
+
+        // rainbow colors
+        newDiv.style.backgroundColor = colorWheel.GetNextColor();
+        newDiv.style.color = colorWheel.TextColor;
+        if (idx === 0)
+            newDiv.style.backgroundColor = colorWheel.ParentColor;
+        
         displayDiv.appendChild(newDiv);
     }
 
+}
+
+function ColorWheel() {
+    this.Index = 0;
+    // https://htmlcolorcodes.com/
+    // red -> purple -> blue -> green -> yellow -> orange
+    this.ParentColor = "#1B2631";
+    this.TextColor = "#F7F9F9";
+    this.Colors = [
+        "#641E16",
+        "#78281F",
+        "#512E5F",
+        "#4A235A",
+        "#154360",
+        "#1B4F72",
+        "#0E6251",
+        "#0B5345",
+        "#145A32",
+        "#186A3B",
+        "#7D6608",
+        "#7E5109",
+        "#784212",
+        "#6E2C00"];
+
+    this.GetNextColor = function() {
+        this.Index = (this.Index + 1) % this.Colors.length;
+        return this.Colors[this.Index];
+    }
 }
