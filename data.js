34c0cb79f86a5fbcaed3a72a7eeae47377f6ef63
dramasawmy <dannyramasawmy@gmail.com>
2022-05-14 08:51:56 +0100

    msg

--- data.js
+++ data.js
@@ -9,7 +9,8 @@ a ă â b c d e f g h i î j k l m n o p q r s ș t ț u v w x y z
 var romanian = new DataCard(
     "Romanian", "Română",
     "Romanian language tree. <br>\
-    Click here to change the langauge.");
+    Click here to change the langauge. \
+    Search is debugging");
 romanian.SetParent(romanian);
     
 // =============================================================================
