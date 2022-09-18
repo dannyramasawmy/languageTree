88609b529d99070d0bdad4c9180a2694fe49b30f
Danny Ramasawmy <dannyramasawmy@gmail.com>
2022-09-18 18:00:27 +0100

    try something

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //
 // Major version: changes in behavior / new buttons etc
 // Minor version: new/edited cars / bug fixes
-const staticCacheName = 'languageTree-v15-2';
+const staticCacheName = 'languageTree-v15-3';
 
 self.addEventListener('install', (e) => {
   console.log("Registering sw");
