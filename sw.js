8f024c4144aaeae39017a4acd779423c8ccccca5
Danny Ramasawmy <dannyramasawmy@gmail.com>
2022-09-18 15:08:31 +0100

    remove icon

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const staticCacheName = 'languageTree-v14-2';
+//
+// Major version: changes in behavior / new buttons etc
+// Minor version: new words added / corrections
+const staticCacheName = 'languageTree-v15-0';
 
 self.addEventListener('install', (e) => {
   console.log("Registering sw");
@@ -7,10 +10,15 @@ self.addEventListener('install', (e) => {
       '/languageTree/',
       '/languageTree/data/romanian-tree.js',
       '/languageTree/src/languageTree.js',
-      '/languageTree/img/delta-icon.png',
       '/languageTree/img/background-tree.svg',
+      '/languageTree/img/delta-icon.png',
       '/languageTree/img/parent-icon.png',
-      '/languageTree/img/shuffle-icon.png',
+      '/languageTree/img/shuffle-icon-1.png',
+      '/languageTree/img/shuffle-icon-2.png',
+      '/languageTree/img/shuffle-icon-3.png',
+      '/languageTree/img/shuffle-icon-4.png',
+      '/languageTree/img/swap-language-icon-1.png',
+      '/languageTree/img/swap-language-icon-2.png',
       '/languageTree/src/ui.js',
       '/languageTree/style/rainbow-cards.css',
       '/languageTree/index.html'
