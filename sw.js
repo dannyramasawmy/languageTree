bc2698f93ee11b8909a067037fb42432f9b70b0b
Danny Ramasawmy <dannyramasawmy@gmail.com>
2022-09-25 15:21:59 +0100

    v19-0

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //
 // Major version: changes in behavior / new buttons etc
 // Minor version: new/edited cars / bug fixes
-const staticCacheName = 'languageTree-v18-0';
+const staticCacheName = 'languageTree-v19-0';
 
 self.addEventListener('install', (e) => {
   console.log("Registering sw");
