06ff6b3520d80206498cfa33b942728d8d294e4f
Danny Ramasawmy <dannyramasawmy@gmail.com>
2023-04-07 14:25:13 +0100

    3 - swap UI without functionality

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //
 // Major version: changes in behavior / new buttons etc
 // Minor version: new/edited words / bug fixes
-const staticCacheName = 'languageTree-v19-7';
+const staticCacheName = 'languageTree-v19-8';
 
 self.addEventListener('install', (e) => {
   console.log("Registering sw");
