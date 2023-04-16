01f9e60f214738163e8a03b6e89b5b5056bfa872
Danny Ramasawmy <dannyramasawmy@gmail.com>
2023-04-16 11:35:03 +0100

    7 - copyable

--- src/languageTree.js
+++ src/languageTree.js
@@ -75,37 +75,53 @@ function VerbTemplate(
     presentWe,
     presentYouPlural,
     presentThey,
-    past, infinitive, relfexivePast = "", reflexiveFuture = "")
+    past,
+    infinitive,
+    relfexivePast = "",
+    reflexiveFuture = "")
 {
+    let columnSettings = "col-lg-3 col-md-6 gap-2 verb";
+
     return (`
-        <h2>Present</h2>
-        ${MakeCopyable(`Eu ${presentI}`)}
-        ${MakeCopyable(`Tu ${presentYou}`)}
-        ${MakeCopyable(`El/Ea ${presentHeShe}`)}
-        ${MakeCopyable(`Noi ${presentWe}`)}
-        ${MakeCopyable(`Voi ${presentYouPlural}`)}
-        ${MakeCopyable(`Ei/Ele ${presentThey}`)}
-        <h2>Past</h2>
-        ${MakeCopyable(`Eu ${relfexivePast} am ${past}`)}
-        ${MakeCopyable(`Tu ${relfexivePast} ai ${past}`)}
-        ${MakeCopyable(`El/Ea ${relfexivePast} a ${past}`)}
-        ${MakeCopyable(`Noi ${relfexivePast} am ${past}`)}
-        ${MakeCopyable(`Voi ${relfexivePast} ați ${past}`)}
-        ${MakeCopyable(`Ei/Ele ${relfexivePast} au ${past}`)}
-        <h2>Future</h2>
-        ${MakeCopyable(`Eu ${reflexiveFuture} voi ${infinitive}`)}
-        ${MakeCopyable(`Tu ${reflexiveFuture} vei ${infinitive}`)}
-        ${MakeCopyable(`El/Ea ${reflexiveFuture} va ${infinitive}`)}
-        ${MakeCopyable(`Noi ${reflexiveFuture} vom ${infinitive}`)}
-        ${MakeCopyable(`Voi ${reflexiveFuture} veți ${infinitive}`)}
-        ${MakeCopyable(`Ei/Ele ${reflexiveFuture} vor ${infinitive}`)}
-        <h2>Conditional mood (you would)</h2>
-        ${MakeCopyable(`Eu aș ${infinitive}`)}
-        ${MakeCopyable(`Tu ai ${infinitive}`)}
-        ${MakeCopyable(`El/Ea ar ${infinitive}`)}
-        ${MakeCopyable(`Noi am ${infinitive}`)}
-        ${MakeCopyable(`Voi ați ${infinitive}`)}
-        ${MakeCopyable(`Ei/Ele ar ${infinitive}`)}
+    <br>
+    <div class="row gy-4">
+        <div class="${columnSettings}">
+            <h4>Present</h4>
+            ${MakeCopyable(`Eu ${presentI}`)}
+            ${MakeCopyable(`Tu ${presentYou}`)}
+            ${MakeCopyable(`El/Ea ${presentHeShe}`)}
+            ${MakeCopyable(`Noi ${presentWe}`)}
+            ${MakeCopyable(`Voi ${presentYouPlural}`)}
+            ${MakeCopyable(`Ei/Ele ${presentThey}`)}
+            </div>
+        <div class="${columnSettings}">
+            <h4>Past</h4>
+            ${MakeCopyable(`Eu ${relfexivePast} am ${past}`)}
+            ${MakeCopyable(`Tu ${relfexivePast} ai ${past}`)}
+            ${MakeCopyable(`El/Ea ${relfexivePast} a ${past}`)}
+            ${MakeCopyable(`Noi ${relfexivePast} am ${past}`)}
+            ${MakeCopyable(`Voi ${relfexivePast} ați ${past}`)}
+            ${MakeCopyable(`Ei/Ele ${relfexivePast} au ${past}`)}
+        </div>
+        <div class="${columnSettings}">
+            <h4>Future</h4>
+            ${MakeCopyable(`Eu ${reflexiveFuture} voi ${infinitive}`)}
+            ${MakeCopyable(`Tu ${reflexiveFuture} vei ${infinitive}`)}
+            ${MakeCopyable(`El/Ea ${reflexiveFuture} va ${infinitive}`)}
+            ${MakeCopyable(`Noi ${reflexiveFuture} vom ${infinitive}`)}
+            ${MakeCopyable(`Voi ${reflexiveFuture} veți ${infinitive}`)}
+            ${MakeCopyable(`Ei/Ele ${reflexiveFuture} vor ${infinitive}`)}
+        </div>
+        <div class="${columnSettings}">
+            <h4>Conditional (you would)</h4>
+            ${MakeCopyable(`Eu aș ${infinitive}`)}
+            ${MakeCopyable(`Tu ai ${infinitive}`)}
+            ${MakeCopyable(`El/Ea ar ${infinitive}`)}
+            ${MakeCopyable(`Noi am ${infinitive}`)}
+            ${MakeCopyable(`Voi ați ${infinitive}`)}
+            ${MakeCopyable(`Ei/Ele ar ${infinitive}`)}
+        </div>
+    </div>
         <br> <br>`);
 }
 
